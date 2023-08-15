2245bd228c51229e4a464fc86be552ac677b9ba3
Thanatip-iceJ <p.thanatip1@gmail.com>
2023-08-15 20:49:49 +0700

    commit from home!!

--- 08-transform-method/01-basicMap/main.js
+++ 08-transform-method/01-basicMap/main.js
@@ -24,6 +24,88 @@ const array06 = [1, -3, 2, 8, -4, 5];
 const newArr06 = array06.map(x => Math.abs(x))
 log(newArr06)
 
+const array07 = [100, 200.25, 300.84, 400.3];
+const newArr07 = array07.map(x => x.toFixed(2))
+log(newArr07)
+
+// const array08 = [0, 5, 10, 7, 6, 5, 0];
+// const newArr08 = array08.map(x => {
+//     let date = new Date()
+//     date.setMonth(x - 1);
+//     console.log(date.setMonth(x - 1)) 
+// })
+// log(newArr08)
+
+// const array09 = [1, 16, 81, 256, 625, 1296];
+// const newArr09 = 
+
+const array10 = [
+    { name: 'apple', age: 14 },
+    { name: 'banana', age: 18 },
+    { name: 'watermelon', age: 32 }
+  ];
+const newArr10 = array10.map(x => x.name)
+log(newArr10)
+
+const array11 = [
+    { name: 'apple', age: 14 },
+    { name: 'banana', age: 18 },
+    { name: 'watermelon', age: 32 }
+  ];
+
+const newArr11 = array11.map(x => x.age)
+log(newArr11)
+
+const array12 = [
+    { name: 'apple', surname: 'London' },
+    { name: 'banana', surname: 'Bangkok' },
+    { name: 'watermelon', surname: 'Singapore' }
+  ];
+
+const newArr12 = array12.map(x => `${x.name} ${x.surname}`)
+log(newArr12)
+
+const array13 = [
+    { name: 'apple', birth: '2000-01-01' },
+    { name: 'banana', birth: '1990-10-01' },
+    { name: 'watermelon', birth: '1985-12-01' }
+  ];
+
+const newArr13 = array13.map(x => {
+    let today = new Date()
+    let birthDate = new Date(x.birth)
+    let age = today.getFullYear() - birthDate.getFullYear() - (today.getMonth() < birthDate.getMonth() || 
+            today.getMonth() == birthDate.getMonth() && today.getDay() < birthDate.getDay())
+    x.age = age;
+    return x;
+})
+
+log(newArr13)
+
+const array14 = [
+    { name: 'apple', birth: '2000-01-01' },
+    { name: 'banana', birth: '1990-10-10' },
+    { name: 'watermelon', birth: '1985-12-30' }
+  ];
+
+const newArr14 = array14.map(x => {
+    let date = new Date(x.birth)
+    let y = date.getFullYear()
+    let m = date.toLocaleString('default', { month: 'short' })
+    let d = date.getDate()
+    let DD_MM_YY = [d, m, y].join(' ')
+    return `<tr><td>${x.name}</td><td>${DD_MM_YY}</td></tr>`    
+})
+
+log(newArr14)
+  
+
+  
+  
+  
+
+
+
 
 
 
